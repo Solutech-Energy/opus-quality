099c56cde8e7837d607a68f00c3a9eac379f71c3
Daniel Rodrigues <daniel.rodrigues@sptech.school>
2023-03-07 22:46:29 +0000

    Sensor Luminosidade em C++

--- sensor-luminosidade.ino
+++ sensor-luminosidade.ino
@@ -1,47 +1,14 @@
-#include <WiFiEsp.h>
+int arduino_pin = A5, leitura_sensor_ldr = 0; //  Lê a entrada do sensor LDR no pino A5 
 
-char ssid[] = "nome_da_rede_wifi"; // nome da rede Wi-Fi
-char pass[] = "senha_wifi"; // senha da rede Wi-Fi
-
-IPAddress server(192, 168, 1, 100); // endereço IP da lâmpada inteligente
-
-WiFiEspClient client; // cria um cliente Wi-Fi
-
-int ldrPin = A0; // pino analógico usado para ler a intensidade da luz
-
-void setup() {
+void setup()  // Configura forma de comunicação serial
+{
   Serial.begin(9600);
-  pinMode(ldrPin, INPUT);
-  WiFi.begin(ssid, pass); // conecta-se à rede Wi-Fi
-
-  while (WiFi.status() != WL_CONNECTED) { // espera a conexão Wi-Fi
-    delay(500);
-    Serial.print(".");
-  }
-  
-  Serial.println("");
-  Serial.println("Conectado à rede Wi-Fi");
+  pinMode(arduino_pin, INPUT);
 }
 
-void loop() {
-  int ldrValue = analogRead(ldrPin); // lê o valor do sensor de luz
-
-  if (ldrValue < 500) { // se a luz estiver fraca
-    if (client.connect(server, 80)) { // conecta-se à lâmpada inteligente
-      client.print("GET /ligar_lampada HTTP/1.1\r\n"); // envia comando para ligar a lâmpada
-      client.print("Host: 192.168.1.100\r\n"); // endereço IP da lâmpada
-      client.print("Connection: close\r\n\r\n");
-      delay(100);
-      client.stop(); // desconecta-se da lâmpada
-    }
-  } else { // se a luz estiver forte
-    if (client.connect(server, 80)) { // conecta-se à lâmpada inteligente
-      client.print("GET /desligar_lampada HTTP/1.1\r\n"); // envia comando para desligar a lâmpada
-      client.print("Host: 192.168.1.100\r\n"); // endereço IP da lâmpada
-      client.print("Connection: close\r\n\r\n");
-      delay(100);
-      client.stop(); // desconecta-se da lâmpada
-    }
-  }
-  delay(1000); // espera 1 segundo antes de fazer a próxima leitura do sensor de luz
+void loop() // Recebe valor do sensor e atribui à variável 'leitura_sensor_ldr'
+{
+  leitura_sensor_ldr = analogRead(arduino_pin);
+  Serial.println(leitura_sensor_ldr);
+  delay(2000);
 }
